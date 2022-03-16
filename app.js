4ae2aaf9dbdd1a2aabf91ae81db515ba3066fa9b
Felipe <fpizarro.ux@gmail.com>
2022-03-16 18:34:45 +0100

    File Upload

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
+const uploadRouter = require('./routes/uploadRouter');
+
 const mongoose = require('mongoose');
 
 const url = config.mongoUrl;
@@ -68,6 +70,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 
